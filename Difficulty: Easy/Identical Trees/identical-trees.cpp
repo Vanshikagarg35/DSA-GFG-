79f2dbb766f1ba30d3ee1108bc384063e0497ab2
Vanshika garg <vnamratagarg35@gmail.com>
2026-06-21 16:11:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.cpp
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -1,84 +1,21 @@
-/*
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int x) {
-        data = x;
-        left = nullptr;
-        right = nullptr;
-    }
-};
-*/
 class Solution {
-  public:
+public:
     bool isIdentical(Node* r1, Node* r2) {
-        vector<int>ans1;
-        vector<int>ans2;
-        queue<Node *>q1;
-        queue<Node *>q2;
-        if(r1!= NULL && r2!=NULL){
-            q1.push(r1);
-            q2.push(r2);
-            ans1.push_back(r1->data);
-            ans2.push_back(r2->data);
+        if (r1 == nullptr && r2 == nullptr) {
+            return true;
         }
-        else{
+        
+        if (r1 == nullptr || r2 == nullptr) {
             return false;
         }
-        while(!q1.empty()){
-            int size = q1.size();
-            for(int i =0; i<q1.size(); i++){
-                Node* temp =  q1.front();
-                q1.pop();
-                Node* left = temp->left;
-                if(left!=NULL){
-                    q1.push(left);
-                    ans1.push_back(left->data);
-                    
-                }
-                else{
-                    ans1.push_back('NULL');
-                    
-                }
-                Node* right = temp->right;
-                if(right!=NULL){
-                    q1.push(right);
-                    ans1.push_back(right->data);
-                }
-                else{
-                    ans1.push_back('NULL');
-                }
-            }
-        }
-        while(!q2.empty()){
-            int size = q2.size();
-            for(int i =0; i<q2.size(); i++){
-                Node* temp =  q2.front();
-                q2.pop();
-                Node* left = temp->left;
-                if(left!=NULL){
-                    q2.push(left);
-                    ans2.push_back(left->data);
-                    
-                }
-                else{
-                    ans2.push_back('NULL');
-                    
-                }
-                Node* right = temp->right;
-                if(right!=NULL){
-                    q2.push(right);
-                    ans2.push_back(right->data);
-                }
-                else{
-                    ans2.push_back('NULL');
-                }
-            }
-        }
-        return ans1 == ans2;
         
+        if (r1->data != r2->data) {
+            return false;
+        }
+        
+        bool leftAns = isIdentical(r1->left, r2->left);
+        bool rightAns = isIdentical(r1->right, r2->right);
+        
+        return leftAns && rightAns;
     }
 };
