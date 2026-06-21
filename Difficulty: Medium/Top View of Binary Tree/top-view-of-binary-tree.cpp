b04534f717bfe918133bb474e144e29ebc32c849
Vanshika garg <vnamratagarg35@gmail.com>
2026-06-21 23:58:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    void find(Node* root, int pos, int& l, int& r) {
+        if (root == nullptr) {
+            return;
+        }
+        l = min(l, pos);
+        r = max(r, pos);
+        find(root->left, pos - 1, l, r);
+        find(root->right, pos + 1, l, r);
+    }
+
+public:
+    vector<int> topView(Node *root) {
+        if (root == nullptr) {
+            return {};
+        }
+        
+        int l = 0, r = 0;
+        find(root, 0, l, r);
+        
+        vector<int> ans(r - l + 1);
+        vector<bool> filled(r - l + 1, false);
+        
+        queue<Node*> q;
+        queue<int> index;
+        
+        q.push(root);
+        index.push(-1 * l);
+        
+        while (!q.empty()) {
+            Node* temp = q.front();
+            q.pop();
+            
+            int pos = index.front();
+            index.pop();
+            
+            if (!filled[pos]) {
+                filled[pos] = true;
+                ans[pos] = temp->data;
+            }
+            
+            if (temp->left != nullptr) {
+                q.push(temp->left);
+                index.push(pos - 1);
+            }
+            if (temp->right != nullptr) {
+                q.push(temp->right);
+                index.push(pos + 1);
+            }
+        }
+        
+        return ans;
+    }
+};
