878ed7b305c84913425ee5701196087193c67368
Vanshika garg <vnamratagarg35@gmail.com>
2026-06-24 01:39:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> inOrder(Node* root) {
+        vector<int> ans;
+        if (root == nullptr) return ans;
+
+        stack<Node*> s;
+        stack<bool> visited;
+
+        s.push(root);
+        visited.push(0);
+
+        while (!s.empty()) {
+            Node* temp = s.top();
+            s.pop();
+            
+            bool flag = visited.top();
+            visited.pop();
+
+            if (!flag) {
+                if (temp->right) {
+                    s.push(temp->right);
+                    visited.push(0);
+                }
+                
+                s.push(temp);
+                visited.push(1);
+                
+                if (temp->left) {
+                    s.push(temp->left);
+                    visited.push(0);
+                }
+            } else {
+                ans.push_back(temp->data);
+            }
+        }
+        
+        return ans;
+    }
+};
