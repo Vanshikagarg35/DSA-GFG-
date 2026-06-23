608ad4898f7e07a4db85a01afe9c14939af4c7ed
Vanshika garg <vnamratagarg35@gmail.com>
2026-06-23 22:48:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -1,54 +1,57 @@
-class Solution {
-    void find(Node* root, int pos, int& l, int& r) {
-        if (root == nullptr) {
-            return;
-        }
-        l = min(l, pos);
-        r = max(r, pos);
-        find(root->left, pos - 1, l, r);
-        find(root->right, pos + 1, l, r);
-    }
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
 
-public:
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+  void find(Node *root, int pos , int &l , int &r){
+      if(!root) return;
+      l = min( l ,pos);
+      r = max(r, pos);
+      find(root->left , pos-1 , l , r);
+      find(root->right , pos+1 , l, r);
+  }
+  
     vector<int> topView(Node *root) {
-        if (root == nullptr) {
-            return {};
-        }
-        
-        int l = 0, r = 0;
-        find(root, 0, l, r);
-        
-        vector<int> ans(r - l + 1);
-        vector<bool> filled(r - l + 1, false);
-        
-        queue<Node*> q;
-        queue<int> index;
-        
+        int l=0;
+        int r=0;
+        find(root , 0 , l, r);
+        vector<int>ans(r-l+1 , 0);
+        vector<bool>filled(r-l+1 , 0);
+        queue<Node* > q;
+        queue<int>index;
         q.push(root);
         index.push(-1 * l);
-        
-        while (!q.empty()) {
+        while(!q.empty()){
             Node* temp = q.front();
             q.pop();
-            
             int pos = index.front();
             index.pop();
-            
-            if (!filled[pos]) {
-                filled[pos] = true;
+            if(!filled[pos]){
+                filled[pos]=1;
                 ans[pos] = temp->data;
             }
-            
-            if (temp->left != nullptr) {
+            if(temp->left){
                 q.push(temp->left);
-                index.push(pos - 1);
+                index.push(pos-1);
             }
-            if (temp->right != nullptr) {
+            if(temp->right){
                 q.push(temp->right);
-                index.push(pos + 1);
+                index.push(pos+1);
             }
         }
-        
         return ans;
+
     }
 };
