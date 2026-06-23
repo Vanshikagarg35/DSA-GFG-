adb822c6879913a31bbead3efd73f196ed2d9f4b
Vanshika garg <vnamratagarg35@gmail.com>
2026-06-23 23:51:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -1,50 +1,47 @@
+/*
+Definition for Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
 class Solution {
-    void find(Node* root, int pos, int& l, int& r) {
-        if (root == nullptr) {
-            return;
+  public:
+    void find(Node* root, int pos , int &l, int &r){
+        if(!root) return;
+        l = min(pos , l);
+        r = max(pos ,r);
+        find(root->left , pos-1 , l ,r);
+        find(root->right , pos+1 , l , r);
+    }
+    vector<int> bottomView(Node *root) {
+        int l=0;
+        int r=0;
+        find(root , 0 ,l, r );
+        vector<int>ans(r-l+1);
+        vector<int>level(r-l+1 , INT_MIN);
+        Bview(root , -1*l ,ans ,level , 0);
+        return ans;
+        
+    }
+    void Bview(Node* root, int pos , vector<int>& ans, vector<int>& level , int l){
+        if(!root) return;
+        if(level[pos]<=l){
+            ans[pos] = root->data;
+            level[pos] = l;
         }
-        l = min(l, pos);
-        r = max(r, pos);
-        find(root->left, pos - 1, l, r);
-        find(root->right, pos + 1, l, r);
+        Bview(root->left , pos-1 , ans , level ,l+1);
+        Bview(root->right , pos+1, ans , level, l+1);
     }
 
-public:
-    vector<int> bottomView(Node *root) {
-        if (root == nullptr) {
-            return {};
-        }
         
-        int l = 0, r = 0;
-        find(root, 0, l, r);
-        
-        vector<int> ans(r - l + 1);
-        
-        queue<Node*> q;
-        queue<int> index;
-        
-        q.push(root);
-        index.push(-1 * l);
-        
-        while (!q.empty()) {
-            Node* temp = q.front();
-            q.pop();
-            
-            int pos = index.front();
-            index.pop();
-            
-            ans[pos] = temp->data;
-            
-            if (temp->left != nullptr) {
-                q.push(temp->left);
-                index.push(pos - 1);
-            }
-            if (temp->right != nullptr) {
-                q.push(temp->right);
-                index.push(pos + 1);
-            }
-        }
-        
-        return ans;
-    }
+    
 };
